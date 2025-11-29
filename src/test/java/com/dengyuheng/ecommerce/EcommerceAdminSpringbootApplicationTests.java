9da92499aab240bff716a0d5b3dcc325d1e52fd5
dengxinyi <1148926496@qq.com>
2025-11-29 19:23:51 +0800

    项目初始化

---
+++ src/test/java/com/dengyuheng/ecommerce/EcommerceAdminSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengyuheng.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAdminSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
